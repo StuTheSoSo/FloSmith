1733516e097109d8e0904aec99126479319bd297
Stu Schwartz <stu.schwartz@gmail.com>
2026-08-12 12:31:03 -0400

    android building

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,14 +1,21 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.flosmith.app',
+  appId: 'com.flosmith.app', // Ensure this matches your package name in the Play Console
   appName: 'FloSmith',
   webDir: 'dist/FloSmithApp/browser',
-  bundledWebRuntime: false,
+  server: {
+    androidScheme: 'https' // Recommended for production
+  },
   plugins: {
     Keyboard: {
       resize: 'none',
       scrollAssist: false
+    }
+  },
+  android: {
+    buildOptions: {
+      releaseType: 'bundle', // Mandatory for new Play Store apps (.aab)
     }
   }
 };
